6ccb915e978dd50dabc2447442918215c6d4d1b3
hung <hn989422@gmail.com>
2024-07-17 22:55:55 +0700

    fullstack

--- giadinhthoxinh/Areas/Seller/Controllers/CategoriesController.cs
+++ giadinhthoxinh/Areas/Seller/Controllers/CategoriesController.cs
@@ -1,41 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using giadinhthoxinh.Areas.Seller.Filter;
 using giadinhthoxinh.Models;
 
-namespace giadinhthoxinh.Areas.Seller.Controllers
-{
-    public class CategoriesController : Controller
-    {
+namespace giadinhthoxinh.Areas.Seller.Controllers {
+    public class CategoriesController : Controller {
         private giadinhthoxinhEntities db = new giadinhthoxinhEntities();
 
         // GET: Seller/Categories
         [SupplierFilter]
-        public ActionResult Index(int? supplierID)
-        {
+        public ActionResult Index(int? supplierID) {
             var userId = (int)Session["idUser"];
             var userSupplierIDs = db.tblSuppliers
-                                    .Where(s => s.FK_iAccountID == userId &&s.iState!=0)
+                                    .Where(s => s.FK_iAccountID == userId && s.iState != 0)
                                     .Select(s => s.PK_iSupplierID)
                                     .ToList();
             var categories = db.tblCategories
                                .Where(c => userSupplierIDs.Contains(c.FK_iSupplierID));
 
-            if (supplierID.HasValue)
-            {
+            if (supplierID.HasValue) {
                 categories = categories.Where(x => x.FK_iSupplierID == supplierID.Value);
             }
 
             var suppliers = db.tblSuppliers
                               .Where(s => s.FK_iAccountID == userId)
-                              .Select(s => new SelectListItem
-                              {
+                              .Select(s => new SelectListItem {
                                   Value = s.PK_iSupplierID.ToString(),
                                   Text = s.sSupplierName
                               }).ToList();
@@ -48,28 +40,23 @@ namespace giadinhthoxinh.Areas.Seller.Controllers
 
 
         // GET: Seller/Categories/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Details(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             tblCategory tblCategory = db.tblCategories.Find(id);
-            if (tblCategory == null)
-            {
+            if (tblCategory == null) {
                 return HttpNotFound();
             }
             return View(tblCategory);
         }
 
         // POST: Seller/Categories/Create
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             var userId = (int)Session["idUser"];
             var suppliers = db.tblSuppliers
-                              .Where(s => s.FK_iAccountID == userId&&s.iState!=0)
-                              .Select(s => new SelectListItem
-                              {
+                              .Where(s => s.FK_iAccountID == userId && s.iState != 0)
+                              .Select(s => new SelectListItem {
                                   Value = s.PK_iSupplierID.ToString(),
                                   Text = s.sSupplierName
                               }).ToList();
@@ -81,12 +68,10 @@ namespace giadinhthoxinh.Areas.Seller.Controllers
         // POST: Seller/Categories/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PK_iCategoryID,sCategoryName")] tblCategory tblCategory)
-        {
+        public ActionResult Create([Bind(Include = "PK_iCategoryID,sCategoryName")] tblCategory tblCategory) {
             var userId = (int)Session["idUser"];
-            var iSupplierID = db.tblSuppliers.Where(x=>x.FK_iAccountID==userId).Select(x=>x.PK_iSupplierID).FirstOrDefault();
-            if (ModelState.IsValid && tblCategory.FK_iSupplierID.ToString() != null && tblCategory.sCategoryName != null)
-            {
+            var iSupplierID = db.tblSuppliers.Where(x => x.FK_iAccountID == userId).Select(x => x.PK_iSupplierID).FirstOrDefault();
+            if (ModelState.IsValid && tblCategory.FK_iSupplierID.ToString() != null && tblCategory.sCategoryName != null) {
                 tblCategory.FK_iSupplierID = iSupplierID;
                 db.tblCategories.Add(tblCategory);
                 db.SaveChanges();
@@ -95,8 +80,7 @@ namespace giadinhthoxinh.Areas.Seller.Controllers
 
             var suppliers = db.tblSuppliers
                               .Where(s => s.FK_iAccountID == userId && s.iState != 0)
-                              .Select(s => new SelectListItem
-                              {
+                              .Select(s => new SelectListItem {
                                   Value = s.PK_iSupplierID.ToString(),
                                   Text = s.sSupplierName
                               }).ToList();
@@ -107,15 +91,12 @@ namespace giadinhthoxinh.Areas.Seller.Controllers
         }
 
         // GET: Seller/Categories/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Edit(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             tblCategory tblCategory = db.tblCategories.Find(id);
-            if (tblCategory == null)
-            {
+            if (tblCategory == null) {
                 return HttpNotFound();
             }
             return View(tblCategory);
@@ -124,10 +105,8 @@ namespace giadinhthoxinh.Areas.Seller.Controllers
         // POST: Seller/Categories/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PK_iCategoryID,FK_iSupplierID,sCategoryName")] tblCategory tblCategory)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Edit([Bind(Include = "PK_iCategoryID,FK_iSupplierID,sCategoryName")] tblCategory tblCategory) {
+            if (ModelState.IsValid) {
                 db.Entry(tblCategory).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -136,15 +115,12 @@ namespace giadinhthoxinh.Areas.Seller.Controllers
         }
 
         // GET: Seller/Categories/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Delete(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             tblCategory tblCategory = db.tblCategories.Find(id);
-            if (tblCategory == null)
-            {
+            if (tblCategory == null) {
                 return HttpNotFound();
             }
             return View(tblCategory);
@@ -152,26 +128,16 @@ namespace giadinhthoxinh.Areas.Seller.Controllers
 
         // POST: Seller/Categories/Delete/5
         [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            var productsWithCategory = db.tblProducts.Where(x => x.FK_iCategoryID == id).ToList();
-            if (productsWithCategory.Any())
-            {
-                return Json(new { success = false, message = "Vui lòng xóa các sản phẩm chứa danh mục này" });
-            }
-            else
-            {
-                tblCategory tblCategory = db.tblCategories.Find(id);
-                db.tblCategories.Remove(tblCategory);
-                db.SaveChanges();
-                return Json(new { success = true, message = "Xóa thành công" });
-            }
+        public ActionResult DeleteConfirmed(int id) {
+
+            tblCategory tblCategory = db.tblCategories.Find(id);
+            db.tblCategories.Remove(tblCategory);
+            db.SaveChanges();
+            return Json(new { success = true, message = "Xóa thành công" });
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
                 db.Dispose();
             }
             base.Dispose(disposing);
